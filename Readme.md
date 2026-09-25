c5e0cbad3b44f949ad1a7d0653a1c9457f4e069b
meryembenassila <benassila.meryem08@gmail.com>
2026-09-25 11:15:23 +0100

    feat: création de l'API REST et tests avec Postman

--- Readme.md
+++ Readme.md
@@ -10,7 +10,7 @@ Ce microservice permet de gérer des comptes bancaires à travers plusieurs type
 - **Spring Data REST**
 - **GraphQL API**
 
-![img.png](img.png)
+![img.png](images/img.png)
 # Étapes de réalisation
 
 ## 1. Création du projet Spring Boot
@@ -19,7 +19,7 @@ La première étape consiste à créer un projet **Spring Boot** avec les dépen
 
 ###  Dépendances utilisées
 
-![img_1.png](img_1.png)
+![img_1.png](images/img_1.png)
 
 
 ---
@@ -32,7 +32,7 @@ Elle est associée à une table dans la base de données grâce à **JPA**.
 
 ###  Structure de l'entité
 
-![img_2.png](img_2.png)
+![img_2.png](images/img_2.png)
 
 ## 3. Création de l'interface `CompteRepository`
 
@@ -41,10 +41,57 @@ Pour accéder aux données de l'entité `BankAccount`, une interface `BankAccoun
 Elle hérite de `JpaRepository`, ce qui permet de bénéficier automatiquement des principales opérations **CRUD**.
 
 
-![img_3.png](img_3.png)
+![img_3.png](images/img_3.png)
 
 ## 4. Test de la couche DAO
 
 La couche **DAO (Data Access Object)** est ensuite testée afin de vérifier que la communication entre l'application et la base de données fonctionne correctement.
 
-![img_4.png](img_4.png)
+![img_4.png](images/img_4.png)
+![img.png](images/img5.png)
+
+## 5. Création du Web Service RESTful
+
+Après avoir mis en place l'entité `BankAccount` et le `BankAccountRepository`, l'étape suivante consiste à créer un **Web Service RESTful** permettant aux clients externes de gérer les comptes bancaires.
+
+Pour cela, un contrôleur REST est créé à l'aide de l'annotation `@RestController`.
+
+    ![img_5.png](img_5.png)
+   ![img_6.png](images/img_6.png)
+
+`
+## 6. Test du microservice avec Postman
+
+Après la création du Web Service REST, le microservice est testé à l'aide de **Postman** afin de vérifier le bon fonctionnement des différentes opérations CRUD.
+
+###  Tests réalisés
+
+Les différentes opérations testées sont :
+
+| Méthode HTTP | Endpoint                 | Description |
+|---|--------------------------|---|
+| `GET` | `/api/bankAccounts`      | Récupérer tous les comptes |
+| `GET` | `/api/bankAccounts/{id}` | Récupérer un compte par son identifiant |
+| `POST` | `/api/bankAccounts`      | Créer un nouveau compte |
+| `PUT` | `/api/bankAccounts/{id}` | Modifier un compte |
+| `DELETE` | `/api/bankAccounts/{id}`      | Supprimer un compte |
+
+###  Création d'un compte
+
+![img_10.png](images/img_10.png)
+
+### Récupération des comptes
+
+![img_8.png](images/img_8.png)
+
+###  Récupération d'un compte
+
+
+![img_7.png](images/img_7.png)
+
+###  Modification d'un compte
+
+![img_3.png](images/img_12.png)
+###  Suppression d'un compte
+
+![img_4.png](images/img_9.png)
--- src/main/java/benassila/meryem/ebankservice/web/BankAccountController.java
+++ src/main/java/benassila/meryem/ebankservice/web/BankAccountController.java
@@ -1,23 +1,61 @@
 package benassila.meryem.ebankservice.web;
 
 import benassila.meryem.ebankservice.entities.BanckAccount;
+import benassila.meryem.ebankservice.enums.AccountType;
 import benassila.meryem.ebankservice.repositories.BankAccountRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
 public class BankAccountController {
    private BankAccountRepository bankAccountRepository;
 
+    @GetMapping("/bankaccounts")
+    private List<BanckAccount> getbankAccounts( ){
+        return bankAccountRepository.findAll();
+    }
+
    @GetMapping("/bankaccounts/{id}")
    private BanckAccount getbankAccount(@PathVariable String id ){
-       return bankAccountRepository.findById(id).orElse(null);
+       return bankAccountRepository.findById(id).orElseThrow(()->new RuntimeException(String.format("Account %s Not Found",id)));
+
    }
+
+    @PostMapping("/bankaccounts")
+    private BanckAccount createAccount(@RequestBody BanckAccount banckAccount){
+        System.out.println("err");
+        System.out.println(banckAccount.getId());
+        if (banckAccount.getId()== null) {
+            banckAccount.setId(UUID.randomUUID().toString());
+        }
+        System.out.println(banckAccount.getId());
+        if (banckAccount.getCreatedAt()== null) banckAccount.setCreatedAt(new Date());
+
+        return bankAccountRepository.save(banckAccount);
+
+    }
+
+    @PutMapping("/bankaccounts/{id}")
+    private BanckAccount updateAccount(@PathVariable String id,@RequestBody BanckAccount banckAccount){//cette fonction pour out et patch
+        BanckAccount account = bankAccountRepository.findById(id).orElseThrow(()->new RuntimeException(String.format("Account %s Not Found",id)));
+        if(banckAccount.getBalance()!=null) account.setBalance(banckAccount.getBalance());//on a odifier double en Double pour valeur null
+        if(banckAccount.getAccountType()!=null) account.setAccountType(banckAccount.getAccountType());
+        if(banckAccount.getCurrency()!=null) account.setCurrency(banckAccount.getCurrency());
+        return bankAccountRepository.save(account);
+
+    }
+
+    @DeleteMapping("/bankaccounts/{id}")
+    private void deletebankAccount(@PathVariable String id ){
+        BanckAccount account = bankAccountRepository.findById(id).orElseThrow(()->new RuntimeException(String.format("Account %s Not Found",id)));
+        bankAccountRepository.deleteById(id);
+
+    }
 
 
 }
